c4ebf0a9a41d8d8e30febba772b055200d392e8b
Lars Westermann <git@lars-westermann.de>
2023-04-29 16:42:38 +0200

    Set base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/v1',
   server: {
     proxy: {
       '/api': 'https://deep.cooking',
