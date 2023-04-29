ee67a4a276845c9ba37752afe75ef09712b2084d
Lars Westermann <git@lars-westermann.de>
2023-04-29 20:25:58 +0200

    Add shadow

--- src/v2/RecipeEditor.tsx
+++ src/v2/RecipeEditor.tsx
@@ -163,6 +163,8 @@ const StyledTagAction = styled.button`
   border: solid 1px var(--border-color);
   flex: 0 0 auto;
   background-color: var(--secondary-background);
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
 
   border-radius: 0.2rem;
 
